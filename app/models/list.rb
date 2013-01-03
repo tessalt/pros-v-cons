ea4a67f823a49669287f3c4210d86f7b0d60364c
Tessa Thornton <tessathornton@gmail.com>
2013-01-02 23:02:21 -0500

    randomize ids

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,16 @@
 class List < ActiveRecord::Base
   attr_accessible :name
+  before_create :randomize_id
 
   validates :name, :presence => true
 
   has_many :points, dependent: :destroy, :order => 'position ASC, created_at DESC'
+
+  private
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(1_000_000)
+    end while List.where(:id => self.id).exists?
+  end    
 
 end
